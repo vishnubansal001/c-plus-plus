237a421860d987cf18eedd5fc4b35589302a867b
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2022-09-13 11:56:50 +0530

    deletion in a linked list implemended

---
+++ linked-list-insertion-deletion-searching/linked-list-deletions.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+class linkedList{
+    public:
+    class Node{
+        public:
+        int data;
+        Node*next;
+    }*head = NULL;
+    void insert(int val){
+        Node* n = new Node;
+        n->data=val;
+        if(head==NULL){
+            head=n;
+            return;
+        }
+        Node*temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+            
+        }
+        temp->next=n;
+    }
+    void display(){
+        Node* ptr;
+        ptr = head;
+        while(ptr!=NULL){
+            cout<<ptr->data<<"->";
+            ptr=ptr->next;
+        }
+        cout<<"NULL"<<endl;
+    }
+    void deletion(int val){
+        Node* temp = head;
+        if(head==NULL){
+            return;
+        }
+        if(head->data==val){
+            Node* todelete1 = head;
+            head = head->next;
+            delete todelete1;
+        }
+        while(temp->next->data!=val){
+            temp=temp->next;
+        }
+        Node* todelete = temp->next;
+        temp->next=temp->next->next;
+        delete todelete;
+    }
+};
+int main()
+{
+    linkedList obj;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        obj.insert(a);
+    }
+    obj.display();
+    int b;
+    cin>>b;
+    obj.deletion(b);
+    obj.display();
+    return 0;
+}
